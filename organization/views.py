04277416c4d5e4c00d83023fe09ec4afa1960410
Marco Montanari <sirmmo@gmail.com>
2011-05-17 16:35:32 +0000

    sections

--- organization/views.py
+++ organization/views.py
@@ -25,7 +25,7 @@ def view_section(request, year, section):
 		'url':str(c.url()),
 		'description':c.description
 	} for c in s.courses.all()]
-	return render_to_response('section.html', {'courses':res, 'section':section})
+	return render_to_response('section.html', {'courses':res, 'section':s.name})
 
 
 
