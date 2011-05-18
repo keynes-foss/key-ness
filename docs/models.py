ff4793969d727bc3fb7d8e31e7d88a08183f538b
Marco Montanari <sirmmo@gmail.com>
2011-05-18 09:16:06 +0200

    admin corrected

--- docs/models.py
+++ docs/models.py
@@ -5,14 +5,18 @@ class Document(models.Model):
         uploader = models.ForeignKey(User)
         description = models.TextField()
         content = models.TextField()
+        filename = models.TextField()
         mime_type = models.TextField()
         private = models.BooleanField(default = True)
         protected = models.BooleanField(default = True)
         uploaded_at = models.DateTimeField(auto_now = True, editable = False)
         last_modified = models.DateTimeField(auto_now_save = True, editable = False)
 
+        def __unicode__(self):
+                return self.filename
+
 class DocumentComment(models.Model):
         document = models.ForeignKey(Document)
         author = models.ForeignKey(User)
         content = models.TextField()
-        date = models.DateTimeField()
+        date = models.DateTimeField(auto_now=True)
